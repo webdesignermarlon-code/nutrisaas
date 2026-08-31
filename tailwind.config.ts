3e416f529f280c11706833b19cdf3150ed84d3dc
Marlon Andrade <seu-email@gmail.com>
2026-08-31 13:05:40 -0300

    Limpeza total de estilos e suporte nativo ao dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  darkMode: 'class', // <--- ESSENCIAL PARA O BOTÃO FUNCIONAR
+  darkMode: 'class', // <--- ESSENCIAL
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
