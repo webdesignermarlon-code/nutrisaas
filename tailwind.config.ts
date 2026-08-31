fee292ea3ea5c3bb52609ace64ac93c3fae0c95f
Marlon Andrade <seu-email@gmail.com>
2026-08-31 12:49:50 -0300

    Corrige alternancia dinamica de temas claro e escuro no dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class', // <--- ESSENCIAL PARA O BOTÃO FUNCIONAR
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,4 +12,5 @@ const config: Config = {
   },
   plugins: [],
 }
+
 export default config
